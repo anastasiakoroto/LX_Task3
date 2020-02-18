30092d5cec2e4cb39124804e55903502b68c4aa1
anastasiakoroto <anastasiakoroto@gmail.com>
2020-02-18 21:18:23 +0300

    Add "a" variable to function parameters to avoid global variables

--- task_3.py
+++ task_3.py
@@ -1,19 +1,17 @@
 from concurrent.futures import ThreadPoolExecutor
 
-a = 0
 
-
-def function(args):
-    global a
+def function(args, a):
     for _ in range(args):
         a += 1
+    return a
 
 
 def main():
-
+    a = 0
     with ThreadPoolExecutor(max_workers=5) as executor:
         for i in range(5):
-            executor.submit(function, 1_000_000).result()
+            a = executor.submit(function, 1_000_000, a).result()
 
     print("----------------------", a)  # 5_000_000
 
